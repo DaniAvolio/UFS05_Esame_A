078d1db9d01bc828bbefa6b6d47459a68fa7b5ef
DaniAvolio <avoliodaniele9@gmail.com>
2022-05-23 14:48:56 +0200

    feat: add function and menu for the client

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import com.google.gson.Gson;
@@ -12,9 +14,41 @@ public class App
     public static void main( String[] args )
     {
         buildProductList();
+        ClientHandler ch = new ClientHandler();
+        //inizializzazione
+        System.out.println("Server Started...");
+        ServerSocket serverSocket = null;
+        //inizializzo serversocket
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            //inizializzo clientsocket e la connessione
 
-
+            System.out.println("Accepting client...");
+            try {
+                assert serverSocket != null;
+                clientSocket = serverSocket.accept();
+                System.out.println("Client Accepted");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //chiamo la funzione per gestire il client
+            ch.ManageClient();
+            //chiamo la funzione per il processing
+            ch.processl();
+            try {
+                clientSocket.close();
+                System.out.println("...Client quit...");
+                System.out.println("Server restarting...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
     static void buildProductList() {
         piatti.add(new Piatto(1, "Spaghetti con le vongole", 19.99, "piatto a base di vongole veraci"));
         piatti.add(new Piatto(2, "Pennette al pomodoro fresco", 9.99, "piatto a base di penne al pomodoro del Vesuvio"));
@@ -23,9 +57,41 @@ public class App
         piatti.add(new Piatto(5, "Tagliatelle alla Bolognese", 14.99, "piatto a base di ragù di carne"));
     }
 
+    //SEZIONE FUNZIONI
 
+    static String all(){
+        Gson gson = new Gson();
+        String result = "";
+        result=gson.toJson(piatti);
+        return result;
+    }
+    static String allsorted(){
+        Gson gson = new Gson();
+        String result = "";
+        piatti.sort((Piatto p1, Piatto p2)-> {
+            return p1.getNome().compareTo(p2.getNome());
+        });
+        result=gson.toJson(piatti);
+        return result;
+    }
+    static String more_expensive(){
+        Gson gson = new Gson();
+        String result = "";
+        int select=0;
+        double max=0;
 
+        for(int i=0;i<piatti.size();i++)
+        {
+            double elemento=piatti.get(i).getPrezzo();
+            if(elemento > max){
+                max = elemento;
+                select=i;
+            }
+        }
+        result= gson.toJson(piatti.get(select));
+        return result;
+}
 
-
+    //FINE SEZIONE FUNZIONI
 
 }
--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,5 +1,74 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static org.example.App.*;
+
+
 public class ClientHandler {
 
+    private static BufferedReader in;
+    private static PrintWriter out;
+
+    public ClientHandler() {
+    }
+
+    public static void ManageClient() {
+        //receive bufferedreader for input
+        in = null;
+        try {
+            assert clientSocket != null;
+            in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //inizializzo printwriter per l output, cioe per la risposta del server
+        out = null;
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Processo : il server riceve la stringa nel client, fa i suoi calcoli e spedisce il risultato
+    public static void processl(){
+        try {
+            out.println("BENVENUTO CLIENT");
+            out.println("\nQuesta è la lista di comandi che puoi digitare :");
+            out.println("all -> per la lista di tutti i piatti presenti nel database");
+            out.println("all_sorted -> per la lista di tutti i piatti presenti nel database in ordinamento AB");
+            out.println("more_expensive -> per il piatto più costoso presente nel database");
+            String s;
+            while ((s = in.readLine()) != null)  {
+                System.out.println("Ricevuto : "+s);
+                switch (s){
+                    case "more_expensive":
+                        out.println(more_expensive());
+                        out.flush();
+                        break;
+
+                    case "all":
+                        out.println(all());
+                        out.flush();
+                        break;
+
+                    case "all_sorted" :
+                        out.println(allsorted());
+                        out.flush();
+                        break;
+                    default:
+                        out.println("Il comando inserito non esiste! Devi inserire uno dei comandi sopraelencati!");
+                        out.flush();
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
